221b44bbf3990ec1202971568c571ee93cfb0178
Aderinsola7 <78368015+Aderinsola7@users.noreply.github.com>
2023-09-07 23:46:56 +0100

    Add files via upload

---
+++ dice roll game.py
@@ -0,0 +1,54 @@
+import random #HELPS GENERATE RANDOM NUMBERS
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+#player input required info for the game
+while True:
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("invalid input,\nmake sure its a number that is between 2 - 4")
+
+#the game system of operation
+max_score = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < max_score:
+
+    for player_idx in range(players):
+        print("\nPlayer", player_idx + 1, "turn has just started!\n")
+        print("Your total score is:", player_score[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled 1! You turn is done")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a: ", value)
+
+            print("Your score is: ", current_score)
+        player_score[player_idx] += current_score
+        print("Your total score is:", player_score[player_idx])
+
+#declare the winner
+max_score = max(player_score)
+winning_index = player_score.index(max_score)
+print("Player number", winning_index+ 1, "is the winner with a score of:", max_score, "\n(^ - ^)")
